2860b307cad1a83c698a10098fe51281b92b4da6
Graeme Congreve <junkmailgmc@gmail.com>
2019-01-19 16:45:59 +0000

    added MusicShop class

--- src/main/java/MusicShop/MusicShop.java
+++ src/main/java/MusicShop/MusicShop.java
@@ -1,4 +1,41 @@
 package MusicShop;
 
+import behaviours.ISell;
+import stockItems.StockItem;
+
+import java.util.ArrayList;
+
 public class MusicShop {
+
+    private ArrayList<ISell> stock;
+    private Till till;
+
+    public MusicShop(ArrayList<ISell> stock) {
+        this.stock = stock;
+        this.till = new Till(20);
+    }
+
+    public ArrayList<ISell> getStock() {
+        return stock;
+    }
+
+    public double getTillTotal() {
+        return till.getCurrentTotal();
+    }
+
+    public void addMoneyToTill(double money){
+        till.addMoney(money);
+    }
+
+    public void removeMoneyFromTill(double money){
+        till.removeMoney(money);
+    }
+
+    public void addItemsToStock(ISell stockItem){
+        this.stock.add(stockItem);
+    }
+
+    public void removeItemFromStock(ISell stockItem){
+        this.stock.remove(stockItem);
+    }
 }
